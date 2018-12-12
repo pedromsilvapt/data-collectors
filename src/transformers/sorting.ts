25b680556cc3504d0758bcf7e1c4286be9674ba9
Pedro M. Silva <pemiolsi@hotmail.com>
2018-12-12 21:27:21 +0000

    Small fix in sorting type declarations

--- src/transformers/sorting.ts
+++ src/transformers/sorting.ts
@@ -2,7 +2,7 @@ import { Collector } from "../collector";
 import { Comparator } from "../queries/math";
 import { toArray } from "../collections/toArray";
 
-export function sorting<T, A, R> ( comparator ?: Comparator<T>, collector ?: Collector<T, A, R> ) : Collector<T, T[], R>;
+export function sorting<T, A, R> ( comparator : Comparator<T>, collector : Collector<T, A, R> ) : Collector<T, T[], R>;
 export function sorting<T, A, R> ( comparator ?: Comparator<T> ) : Collector<T, T[], T[]>;
 export function sorting<T, A, R> ( comparator ?: Comparator<T>, collector ?: Collector<T, A, R> ) : Collector<T, T[], R> {
     return toArray<T>().then( arr => {
